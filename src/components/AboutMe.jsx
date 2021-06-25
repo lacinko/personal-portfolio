cc738d47ae738d3ac8d55041227d6d55770d7ac1
Ladislav <ladislav.topolsky@gmail.com>
2021-06-25 22:49:23 +0200

    Update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -10,11 +10,7 @@ export const AboutMe = () => {
     <section className="section about-me" id="about-me">
       <h4>About Me</h4>
       <div className="about-me__container">
-        <img
-          src="https://lh3.googleusercontent.com/0WIsRkl0aJphLVwA5-lOfF7HhEMZ0IEjpm9X30nTVB8caOLgyihkCTEkqRlP4SN9VeI0vP6Fu2aUv1qrUcE6zoTAdkPLkaaTJ5rR6KXSjaJXMDNLL1tsvIf8x5X6xEhKLZQPBK6RlN93g-fX2uCW59KeH-aCnn1hC5uC2BbeUF_6A6JGfGJ_r0SqyLhxI88v2ihFwG-cGWi60sXcrqUWakduBG9IDfoL57Koialmtg_FxIj_u7IshunFf_Omc3S8hDr3vTHLupjTvAvObHQ3eM4eCURTRswb7ffBSISNsNsmgqTmRFjiXCxsA30AJaQy9oQXvI4O9FQTJdKsi-8SCW2HIBXILRTeX3rFmsiCUMwNcvgZ-4HzQAjA06n0mweyZYVJxYBMKYW0qrUKeFUoWY_iyQ4v5rI3zQ6yDLEAKOij2dzaZTMtnoIMQINhT1oUnD3zW04k-Qs7DpaZX0aWkBzN7WGB4WzGAQxqadgpkXNTODBtH1qzlZZty28ocdMuER4L9b1nrbdqMq0kX3qJonOdnr0UpgAvDJN0_asoPKy3iMXToRU47Jz08KG42gqD6Go2KDBudC6ppPRoV0Th83WewfIScc8jBFM6pYdtIU7567CexVAG6cKIuOA7d-hcDIxAymBRQYo2NfOk7S4DlaL93FelfNDp-QUNdtEFGbfFWSTCE5UTvro4AWqHWtFhHNzGK_nCMkrNMwfJUxCDAM1i=w733-h937-no?authuser=0"
-          alt=""
-          srcset=""
-        />
+        <img src="./profile-picture.png" alt="" srcset="" />
         <div className="about-me__background"></div>
         <ul class="section__list">
           <li className="section__list-item">
